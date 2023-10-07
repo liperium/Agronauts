ba70c961598f7d8767fc219deb4fe48534757495
Memoh <CraftMan3512@gmail.com>
2023-10-07 17:24:10 -0400

    WORKABLE MENU!!

--- game_scenes/UI/BtnShowHideMenu.cs
+++ game_scenes/UI/BtnShowHideMenu.cs
@@ -2,10 +2,31 @@ using Godot;
 
 public partial class BtnShowHideMenu : TextureButton
 {
+    [Export] AnimationTree animTree;
+
+    private bool opened;
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        animTree.AnimationFinished += name =>
+        {
+            Disabled = false;
+        };
+    }
+
     public override void _Pressed()
     {
         base._Pressed();
         
-        //TODO ANIM
+        GD.Print("Menu Open/Close");
+
+        opened = !opened;
+        
+        animTree.Set("parameters/conditions/open", opened);
+        animTree.Set("parameters/conditions/close", !opened);
+        
+        //this.Disabled = true;
+
     }
 }
