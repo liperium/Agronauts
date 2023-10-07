df1e8a5a5a260840a55f8aebb3d3d236ff16aff1
Memoh <Craftman3512@gmail.com>
2023-10-06 20:17:34 -0400

    SAVE WORK

--- GameState/GameState.cs
+++ GameState/GameState.cs
@@ -1,9 +1,80 @@
 using Godot;
 using System;
+using System.IO;
+using System.Text.Json.Serialization;
+using TinyJson;
+using FileAccess = System.IO.FileAccess;
 
-public partial class GameState : Node
+[Serializable]
+public partial class GameState
 {
-    
-    
-    
+	private static GameState _instance;
+	public static GameState instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				if (LoadGame())
+				{
+					return _instance;
+				}
+				
+				return NewGame();
+			}
+			return NewGame();
+		}
+		set => _instance = value;
+	}
+
+	const string filePath = "Save.sav";
+
+	public int randomSeed = 0xBADF00D;
+	
+	public void SaveToFile()
+	{
+		string saveData = this.ToJson();
+		
+		// Write the string array to a new file named "WriteLines.txt".
+		using (StreamWriter outputFile = new StreamWriter(filePath, new FileStreamOptions{ Mode = FileMode.Create, Access = FileAccess.Write}))
+		{
+			outputFile.WriteLine(saveData);
+		}
+	}
+
+	public static bool LoadGame()
+	{
+		try
+		{
+			// Open the text file using a stream reader.
+			using (var sr = new StreamReader(filePath))
+			{
+				// Read the stream as a string, and write the string to the console.
+				GameState newState;
+				newState = sr.ReadToEnd().FromJson<GameState>();
+				instance = newState;
+
+				return true;
+			}
+		}
+		catch (IOException e)
+		{
+			//ERROR
+			GD.Print(e.Message);
+		}
+
+		return false;
+	}
+
+	public static GameState NewGame()
+	{
+		GameState newInstance = new GameState();
+		
+		Random random = new Random();
+		newInstance.randomSeed = random.Next();
+
+		GameState.instance = newInstance;
+		
+		return newInstance;
+	}
 }
---
+++ GameState/SaveManager.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class SaveManager : Node
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        GameState state = GameState.instance;
+        state.SaveToFile();
+    }
+}
