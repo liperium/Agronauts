37b26cbacd95b12fc11bf97d93db80719c8e7fc4
Antoine T-S <anto291100@gmail.com>
2023-10-06 20:47:19 -0400

    to pull

--- game_scenes/farm/UpgradeFarmYieldButton.cs
+++ game_scenes/farm/UpgradeFarmYieldButton.cs
@@ -3,6 +3,13 @@ using System;
 
 public partial class UpgradeFarmYieldButton : Button
 {
+
+	TotalPotatoYieldUpgrade potatoYieldUpgrade;
+
+	public override void _Ready()
+	{
+
+	}
 	public override void _Pressed()
 	{
 		GD.Print("UpgradeFarmYieldButtonPressed");
