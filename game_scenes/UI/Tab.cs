ea8fb76c4706e18eec27ea6fd157298a166b795d
Memoh <CraftMan3512@gmail.com>
2023-10-08 10:55:38 -0400

    tabs work!

--- game_scenes/UI/Tab.cs
+++ game_scenes/UI/Tab.cs
@@ -6,13 +6,18 @@ public partial class Tab : TextureButton
     [Export] public int tabIndex;
     [Export] public TabContainer tabContainer;
     [Export] public TextureRect bg;
-    [Export] public Texture bgTexture;
+    [Export] public Texture2D bgTexture;
     public override void _Pressed()
     {
         base._Pressed();
         if (tabContainer != null)
         {
             tabContainer.CurrentTab = tabIndex;
+        }
+
+        if (bg != null)
+        {
+            bg.Texture = bgTexture;
         }
     }
 }
