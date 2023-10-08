7e9e5300c988afae73a800675448550ef67f0374
Memoh <CraftMan3512@gmail.com>
2023-10-08 14:19:25 -0400

    win condition je crois

---
+++ game_scenes/WinLoader.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class WinLoader : Node2D
+{
+    [Export] public PackedScene winScene;
+    public override void _Ready()
+    {
+        base._Ready();
+        GameState.instance.SetOnWin(OnWin);
+    }
+
+    private void OnWin()
+    {
+        if (winScene != null)
+        {
+            GetTree().ChangeSceneToPacked(winScene);
+        }
+    }
+}
