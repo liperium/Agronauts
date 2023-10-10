979a71c5a81db9aa7388efbb86c58ad631377f8d
Memoh <CraftMan3512@gmail.com>
2023-10-10 11:05:19 -0400

    clean event lifecycle first pass

--- UI/PotatoCountLabel.cs
+++ UI/PotatoCountLabel.cs
@@ -18,4 +18,10 @@ public partial class PotatoCountLabel : Node
     {
         GetParent<LerpValue>().SetNewValue(newValue);
     }
+    
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        number.ResetOnValueChanged(ValueChanged);
+    }
 }
