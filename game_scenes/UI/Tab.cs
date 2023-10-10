da6cde2db967ead77313891ab4b9e4e997e77ca7
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2023-10-10 00:05:22 -0400

    Unused code

--- game_scenes/UI/Tab.cs
+++ game_scenes/UI/Tab.cs
@@ -1,23 +1,16 @@
 using Godot;
 using System;
 
-public partial class Tab : TextureButton
+public partial class Tab : BaseButton
 {
     [Export] public int tabIndex;
     [Export] public TabContainer tabContainer;
-    [Export] public TextureRect bg;
-    [Export] public Texture2D bgTexture;
     public override void _Pressed()
     {
         base._Pressed();
         if (tabContainer != null)
         {
             tabContainer.CurrentTab = tabIndex;
-        }
-
-        if (bg != null)
-        {
-            bg.Texture = bgTexture;
         }
     }
 }
