0b0de5683aa7db1ce11d56ddcdee014c53f7ca6d
Memoh-Laptop <CraftMan3512@gmail.com>
2023-10-30 21:03:15 -0400

    fight lose screen!!

---
+++ game_scenes/fight/ui/FightLoseScreen.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+public partial class FightLoseScreen : Control
+{
+    [Export] public RichTextLabel fightResultDescLabel;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        long cookedPotatoAmountLost = FightManager.instance.cachedHP;
+        fightResultDescLabel.Text = string.Format(Tr("KLOSEDESC"), cookedPotatoAmountLost);
+
+        FightManager.instance.cachedHP = 0;
+    }
+}
